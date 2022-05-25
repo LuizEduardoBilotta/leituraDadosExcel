889931e0311839773299aa79fd0574767d84ce2c
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2022-05-25 19:02:11 -0300

    Refatorando código.

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const readXlsxFile = require('read-excel-file/node');
 
 //Realizando leitura dos dados
-readXlsxFile('./files/dados.xlsx').then(rows => {
-  for (const linha of rows) {
+readXlsxFile('./files/dados.xlsx').then(linhas => {
+  console.log(linhas);
+});
+
+//Realizando leitura e iterando sobre os dados
+readXlsxFile('./files/dados.xlsx').then(linhas => {
+  for (const linha of linhas) {
     console.log(linha);
   }
 });
 
 //Realizando leitura dos nomes das planilhas
-readXlsxFile('./files/dados.xlsx', { getSheets: true }).then(sheets => {
-  console.log(sheets);
+readXlsxFile('./files/dados.xlsx', { getSheets: true }).then(planilhas => {
+  console.log(planilhas);
 });
 
 //Realizando leitura de dados de uma planilha específica
-readXlsxFile('./files/dados.xlsx', { sheet: 'Endereços' }).then(rows => {
-  for (const linha of rows) {
+readXlsxFile('./files/dados.xlsx', { sheet: 'Endereços' }).then(linhas => {
+  for (const linha of linhas) {
     console.log(linha);
   }
 });
